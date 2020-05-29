d7a3c2cf63e209fe9b83c57c1e5ea79268c5f7b7
ddxs <daigang0701@gmail.com>
2020-05-28 20:24:11 -0400

    Update autoexec.sh

--- autoexec.sh
+++ autoexec.sh
@@ -6,3 +6,11 @@ sudo apt install openvpn
 sudo /etc/init.d/openvpn restart
 sudo /etc/init.d/danted restart
 sudo /etc/init.d/cron restart
+
+sudo rm -rf /etc/apt/sources.list.d/* 
+sudo rm -rf /usr/share/dotnet /usr/local/lib/android /opt/ghc &
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install $(curl -fsSL git.io/depends-ubuntu-1804)
+sudo -E apt-get -qq upgrade
+sudo -E apt-get -qq autoremove --purge
+sudo -E apt-get -qq clean
